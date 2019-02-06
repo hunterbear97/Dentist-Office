19ab60806369d4ae1e216d3c1eecbf2782186a8a
hunterbear97 <47253719+hunterbear97@users.noreply.github.com>
2019-02-06 07:10:44 -0700

    Add files via upload

--- FutureAppointment.java
+++ FutureAppointment.java
@@ -2,61 +2,64 @@ package edu.neumont.lytle.dentistoffice.models;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FutureAppointment extends Appointment{
-	private Patient patient;
-	private List<Provider> providers = new ArrayList<>();
-	private List<Procedure> procedures = new ArrayList<>();
+	//private Patient patient;
+//	private List<Provider> providers = new ArrayList<>();
+//	private List<Procedure> procedures = new ArrayList<>();
 	
-	public FutureAppointment() {}
+	private Map<Provider, List<Procedure>> proceduresByProvider = new HashMap<>();
 	
+	public FutureAppointment() {
+		
+	}
+
 	
-	
-	
-	public FutureAppointment(LocalDateTime appointmentDate, Patient patient, List<Provider> providers,
-			List<Procedure> procedures) {
-		super(appointmentDate);
-		this.setPatient(patient);
-		this.setProviders(providers);
-		this.setProviders(providers);
+	public FutureAppointment(LocalDateTime appointmentDate, Patient patient) {
+		super(appointmentDate, patient);
+		// TODO Auto-generated constructor stub
 	}
 
 
 
 
-	public Patient getPatient() {
-		return patient;
-	}
-	public void setPatient(Patient patient) {
-		if(patient == null) {
-			throw new IllegalArgumentException("\"patient\" cannot be null");
-		}
-		this.patient = patient;
-	}
+
+
+	//	@SuppressWarnings("unchecked")
 	public List<Provider> getProviders() {
-		return providers;
+		//List<Provider> providers = new ArrayList<>();
+		
+		return new ArrayList<>(proceduresByProvider.keySet());
 	}
-	public void setProviders(List<Provider> providers) {
-		if(providers == null || providers.isEmpty()) {
-			throw new IllegalArgumentException("\"providers\" cannot be null or empty");
-		}
-		this.providers = providers;
+	
+	public List<Procedure> getProcedures(Provider provider) {
+		return proceduresByProvider.get(provider);
 	}
-	public List<Procedure> getProcedures() {
-		return procedures;
-	}
-	public void setProcedures(List<Procedure> procedures) {
+	public void setProcedures(List<Procedure> procedures, Provider provider) {
 		if(procedures == null || procedures.isEmpty()) {
 			throw new IllegalArgumentException("\"procedures\" cannot be null or empty");
 		}
-		this.procedures = procedures;
+		this.proceduresByProvider.put(provider, procedures);
 	}
 	
-	@Override
+	
 	public String toString() {
-		return super.toString() + ", Patient: " + this.getPatient() + ", " + this.getProviders().toString() + ", Providers: " + this.getProcedures().toString();
+		StringBuilder sb = new StringBuilder();
+		List<Provider> p = this.getProviders();
+		for(int i = 0; i < proceduresByProvider.size(); i++) {
+			sb.append("Provider: ").append(p.get(i)).append(", Procedures: ").append(proceduresByProvider.get(p.get(i)).toString()).append("\n");
+		}
+		
+		return sb.toString();
+		
 	}
+//	@Override
+//	public String toString() {
+//		return super.toString() + " , " + this.getProviders().toString() + ", Providers: " + this.getProcedures().toString();
+//	}
 	
 	
 }
